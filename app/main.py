321afbca0f0f639afa1ba147cd60fe0ee57011be
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2023-04-30 16:50:39 -0700

    upd

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ import sourmash
 app = FastAPI()
 
 class Query(BaseModel):
-    data: bytes # bytes
+    signature: str #json str
 
 def sketch_file_to_sourmash(sequence_file: str):
     """Generate a sourmash sketch from a file of sequences"""
@@ -26,30 +26,26 @@ def sketch_file_to_sourmash(sequence_file: str):
     ss = sourmash.SourmashSignature(sketch)
     return ss
 
-def serialize_sketch(ss) -> bytes:
+def serialize_sig(ss): # -> bytes:
     """Serialize a sourmash sketch into bytes"""
     buf = BytesIO()
     sourmash.save_signatures([ss], buf, compression=True)
     print(f"serialized sourmash signature into {len(buf.getvalue())} bytes.")
     return buf.getvalue()
 
-@app.post("/query/")
-async def create_query_from_file(file: UploadFile):
-    """Testing Version: Upload a file of sequences and query mastiff"""
-    ### this part should be able to be replaced with JS frontend
-    # create a temporary file and save the uploaded file's contents to it
+# receive file, build sourmash sketch, query mastiff
+@app.post("/query_from_file/")
+async def query_mastiff_from_file(file: UploadFile):
+    """Testing Entrypoint: Upload a file of sequences and query mastiff"""
     with NamedTemporaryFile(delete=False) as temp:
         contents = await file.read()
         temp.write(contents)
     sig = sketch_file_to_sourmash(temp.name)
     # delete the temporary file
     os.unlink(temp.name)
-    buf = serialize_sketch(sig)
-    ### end of JS frontend replacement
-    
     http = urllib3.PoolManager()
     try:
-        #buf_value = serialize_sketch(query.sig)
+        buf= serialize_sig(sig)
         r = http.request('POST',
                          'https://mastiff.sourmash.bio/search',
                          body=buf,
@@ -65,3 +61,38 @@ async def create_query_from_file(file: UploadFile):
     json_data = json.dumps(results)
     print(f"Number of matches: {len(results)}")
     return {"result": json_data}
+
+# with data from React frontend
+# query mastiff with a sourmash sketch
+@app.post("/query_mastiff/")
+async def query_mastiff(query: Query):
+    """Query mastiff""" 
+    http = urllib3.PoolManager()
+    try:
+        json_sig = json.loads(query.signature)
+        buf= serialize_sig(json_sig)
+        r = http.request('POST',
+                         'https://mastiff.sourmash.bio/search',
+                         body=buf,
+                         headers={'Content-Type': 'application/json'})
+    except urllib3.exceptions.RequestError as e:
+        raise HTTPException(status_code=500, detail="Server error")
+
+    query_results_text = r.data.decode('utf-8')
+    # load results into a csv reader
+    csv_reader = csv.DictReader(StringIO(query_results_text))
+    results = [row for row in csv_reader]
+    # dump results to json
+    json_data = json.dumps(results)
+    print(f"Number of matches: {len(results)}")
+    return {"result": json_data}
+
+# once working, use functions to read from either file or string json (from react)
+# and only one endpoint.
+# @app.post('/query_from_sig/')
+# async def query_from_sig(query: QuerySignature):
+# #    buf = serialize_sig(query.sig)
+#     mastiff_result = await query_mastiff(sig)
+#     #return {'message': 'File received'}
+#     return mastiff_result 
+
